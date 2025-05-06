3fa9380fd92e2d2470d0024a13006f545301f8f2
gtonu <sanzidtonu@gmail.com>
2025-05-07 02:17:20 +0600

    added new file

---
+++ codeforces/A. Bit++/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int x = 0;
+int n = int.Parse(Console.ReadLine());
+
+for(int i=0; i<n; i++)
+{
+    string operation = Console.ReadLine();
+    if (operation[1] == '+')
+        x++;
+    else
+        x--;
+
+}
+Console.Write(x);
