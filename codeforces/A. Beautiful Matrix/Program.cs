8eb3e40c677d2b48d9d741694cf5459cd0798ea6
gtonu <sanzidtonu@gmail.com>
2025-05-07 02:44:07 +0600

    new file added

---
+++ codeforces/A. Beautiful Matrix/Program.cs
@@ -0,0 +1,24 @@
+﻿
+int middleXcoOrdinate = 3;
+int middleYcoOrdinate = 3;
+
+int xCoOrdinate = 0;
+int yCoOrdinate = 0;
+
+for(int i=1; i<=5; i++)
+{
+    string[] elements = Console.ReadLine().Split(' ');
+    for (int j=0; j<elements.Length; j++)
+    {
+        if (elements[j] == "1")
+        {
+            xCoOrdinate = i;
+            yCoOrdinate = j + 1;
+        }
+    }
+}
+
+int xMoves = middleXcoOrdinate - xCoOrdinate;
+int yMoves = middleYcoOrdinate - yCoOrdinate;
+
+Console.Write(Math.Abs(xMoves) + Math.Abs(yMoves));
