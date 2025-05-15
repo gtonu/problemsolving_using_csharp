5fc1a12fe7615e591a043a5caecd26705fabde8a
gtonu <sanzidtonu@gmail.com>
2025-05-15 16:35:33 +0600

    new file added

---
+++ codeforces/A. Dinner Time/Program.cs
@@ -0,0 +1,23 @@
+﻿int t = int.Parse(Console.ReadLine());
+
+while(t > 0)
+{
+    string[] values = Console.ReadLine().Split(' ');
+
+    int n = int.Parse(values[0]);
+    int m = int.Parse(values[1]);
+    int p = int.Parse(values[2]);
+    int q = int.Parse(values[3]);
+
+    if (n % p == 0 && (q * n) / p != m)
+        Console.WriteLine("NO");
+    else
+        Console.WriteLine("YES");
+
+
+
+
+
+
+        t--;
+}
