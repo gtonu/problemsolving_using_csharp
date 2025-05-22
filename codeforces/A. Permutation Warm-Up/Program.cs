e9f6bc0b4be920dbb02ed38e072817c980df5fef
gtonu <sanzidtonu@gmail.com>
2025-05-23 04:19:15 +0600

    added new files

---
+++ codeforces/A. Permutation Warm-Up/Program.cs
@@ -0,0 +1,10 @@
+﻿
+int t = int.Parse(Console.ReadLine());
+while(t>0)
+{
+    decimal n = decimal.Parse(Console.ReadLine());
+    Console.WriteLine(Math.Floor((n * n) / 4) + 1);
+
+
+    t--;
+}
---
+++ codeforces/A. Three Decks/Program.cs
@@ -0,0 +1,24 @@
+﻿int t = int.Parse(Console.ReadLine());
+
+while(t>0)
+{
+    string[] values = Console.ReadLine().Split(' ');
+    int a = int.Parse(values[0]);
+    int b = int.Parse(values[1]);
+    int c = int.Parse(values[2]);
+
+    if((a+b+c)%3 != 0)
+    {
+        Console.WriteLine("NO");
+    }
+    else
+    {
+        if ((a > (a + b + c) / 3) || (b > (a + b + c) / 3))
+            Console.WriteLine("NO");
+        else
+            Console.WriteLine("YES");
+    }
+
+
+        t--;
+}
