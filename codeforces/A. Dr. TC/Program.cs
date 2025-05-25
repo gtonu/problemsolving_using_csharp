32d2e318947111d57299612861d2e95247a84fe5
gtonu <sanzidtonu@gmail.com>
2025-05-25 22:04:07 +0600

    new files added

---
+++ codeforces/A. Dr. TC/Program.cs
@@ -0,0 +1,29 @@
+﻿int t = int.Parse(Console.ReadLine());
+
+while(t>0)
+{
+    int n = int.Parse(Console.ReadLine());
+    string s = Console.ReadLine();
+
+    int oCounter = 0;
+    int ones = 0;
+
+    for(int i=0; i<s.Length; i++)
+    {
+        if (s[i] == '1')
+            oCounter++;
+    }
+    
+    for(int i=0;  i<s.Length; i++)
+    {
+        if (s[i] == '1')
+            ones += oCounter - 1;
+        else
+            ones += oCounter + 1;
+    }
+    Console.WriteLine(ones);
+
+
+
+    t--;
+}
---
+++ codeforces/A. Vadim's Collection/Program.cs
@@ -0,0 +1,42 @@
+﻿int t = int.Parse(Console.ReadLine());
+
+while(t>0)
+{
+    string number = Console.ReadLine();
+    int[] counter = new int[number.Length];
+    char[] chars = number.ToCharArray();
+
+    for(int i=0; i<number.Length; i++)
+    {
+        int v = int.Parse(number[i].ToString());
+        counter[v]++;
+        
+    }
+    //for (int i = 0; i < counter.Length; i++)
+    //{
+    //    Console.WriteLine(counter[i]);
+    //}
+    for (int i = 1; i < number.Length; i++)
+    {
+        
+        
+            for (int j = (10 - (i + 1)); j >= 0; j--)
+            {
+                if (counter[j] > 0)
+                {
+                    if()
+
+                }
+            }
+        
+    }
+
+    for(int i=0; i<chars.Length; i++)
+    {
+        Console.Write(chars[i]);
+    }
+    Console.WriteLine();
+
+
+    t--;
+}
