a065875b44ef2611e6f56bd0f813f6aa6b5139b3
gtonu <sanzidtonu@gmail.com>
2025-07-01 22:06:13 +0600

    modified file

--- codeforces/A. Vanya and Fence/Program.cs
+++ codeforces/A. Vanya and Fence/Program.cs
@@ -1,20 +1,17 @@
-﻿//string[] values = Console.ReadLine().Split(' ');
-//int n = int.Parse(values[0]);
-//int h = int.Parse(values[1]);
+﻿string[] values = Console.ReadLine().Split(' ');
+int n = int.Parse(values[0]);
+int h = int.Parse(values[1]);
 
-//int[] heights = new int[n];
-//string[] height = Console.ReadLine().Split(' ');
-//int count = 0;
-//for(int i=0;i<n;i++)
-//{
-//    heights[i] = int.Parse(height[i]);
-//    if (heights[i] > h)
-//        count += 2;
-//    else
-//        count++;
-//}
-//Console.WriteLine(count);
+int[] heights = new int[n];
+string[] height = Console.ReadLine().Split(' ');
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+    heights[i] = int.Parse(height[i]);
+    if (heights[i] > h)
+        count += 2;
+    else
+        count++;
+}
+Console.WriteLine(count);
 
-int x = 5;
-int y = x++ + ++x;
-Console.WriteLine(y);
