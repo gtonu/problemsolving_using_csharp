eb217d290cfa5b77e595f6aa48f28cccae67670c
gtonu <sanzidtonu@gmail.com>
2025-07-04 02:54:48 +0600

    added new files

---
+++ codeforces/A. Beautiful Year/Program.cs
@@ -0,0 +1,17 @@
+﻿int year = int.Parse(Console.ReadLine());
+bool flag = true;
+
+while(flag==true)
+{
+    //int n = year;
+    year++;
+    int r1 = year % 10;
+    int r2 = (year / 10) % 10;
+    int r3 = (year / 100) % 10;
+    int r4 = (year / 1000) % 10;
+
+    if ((r1 != r2 && r1 != r3 && r1 != r4) && (r2 != r3 && r2 != r4) && (r3 != r4))
+        break;
+    
+}
+Console.WriteLine(year);
