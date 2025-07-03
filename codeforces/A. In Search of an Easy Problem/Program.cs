f5fe005f16d126c7c415056972a03313b829cbab
gtonu <sanzidtonu@gmail.com>
2025-07-04 00:52:24 +0600

    added new files

---
+++ codeforces/A. In Search of an Easy Problem/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+string[] values = Console.ReadLine().Split(' ');
+int[] opinion = new int[n];
+int count = 0;
+for(int i =0; i<values.Length; i++)
+{
+    opinion[i] = int.Parse(values[i]);
+    if (opinion[i] == 1) count++;
+}
+if (count > 0) Console.WriteLine("HARD");
+else Console.WriteLine("EASY");
---
+++ codeforces/A. Tram/Program.cs
@@ -0,0 +1,15 @@
+﻿int t = int.Parse(Console.ReadLine());
+int capacity = -1;
+int max = 0;
+while(t>0)
+{
+    string[] values = Console.ReadLine().Split(' ');
+    int a = int.Parse(values[0]);
+    int b = int.Parse(values[1]);
+    max -= a;
+    max += b;
+
+    if (max > capacity) capacity = max;
+    t--;
+}
+Console.WriteLine(capacity);
