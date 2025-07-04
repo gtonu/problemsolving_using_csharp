db33a8590570a756099e9e819f831c85b7fd783b
gtonu <sanzidtonu@gmail.com>
2025-07-04 23:06:42 +0600

    added new files

---
+++ codeforces/A. George and Accommodation/Program.cs
@@ -0,0 +1,13 @@
+﻿int t = int.Parse(Console.ReadLine());
+int rooms = 0;
+while(t>0)
+{
+    string[] values = Console.ReadLine().Split(' ');
+    int p = int.Parse(values[0]);
+    int q = int.Parse(values[1]);
+    if ((q - p) >= 2)
+        rooms++;
+
+    t--;
+}
+Console.WriteLine(rooms);
---
+++ codeforces/A. Magnets/Program.cs
@@ -0,0 +1,23 @@
+﻿int t = int.Parse(Console.ReadLine());
+char c = 'a';
+int count = 0;
+while(t>0)
+{
+    string s = Console.ReadLine();
+    if (c == '0' || c == '1')
+    {
+        if (s[0] == c)
+        {
+            count++;
+            c = s[1];
+        }
+        else
+        {
+            c = s[1];
+        }
+    }
+    else
+        c = s[1];
+        t--;
+}
+Console.WriteLine(count+1);
