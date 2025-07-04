e8ada081978d0f6d45ca23f37415c462c65e09a4
gtonu <sanzidtonu@gmail.com>
2025-07-05 03:45:29 +0600

    added new files

---
+++ codeforces/A. Calculating Function/Program.cs
@@ -0,0 +1,18 @@
+﻿long n = long.Parse(Console.ReadLine());
+
+if(n%2==0)
+{
+    long nEven = n / 2;
+    long nOdd = n / 2;
+    long sumEven = (nEven * (nEven + 1));
+    long sumOdd = (nOdd * nOdd);
+    Console.WriteLine(sumEven - sumOdd);
+}
+else
+{
+    long nOdd = (n + 1) / 2;
+    long nEven = n - nOdd;
+    long sumEven = (nEven * (nEven + 1));
+    long sumOdd = (nOdd * nOdd);
+    Console.WriteLine(sumEven - sumOdd);
+}
---
+++ codeforces/B. Drinks/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = int.Parse(Console.ReadLine());
+string[] values = Console.ReadLine().Split(' ');
+int sum = 0;
+for(int i=0;i<values.Length;i++)
+{
+    sum += int.Parse(values[i]);
+}
+
+
+decimal ans = (decimal)sum / (decimal)n;
+Console.WriteLine(ans.ToString("F12"));
