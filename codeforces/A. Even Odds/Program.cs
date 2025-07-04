896a1ee54a08f9716a016a7f8c1127bb4f882d5e
gtonu <sanzidtonu@gmail.com>
2025-07-05 04:15:22 +0600

    added new files

---
+++ codeforces/A. Even Odds/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] values = Console.ReadLine().Split(' ');
+long n = long.Parse(values[0]);
+long k = long.Parse(values[1]);
+if(n%2==0)
+{
+    if ((n / 2) >= k)
+    {
+        Console.WriteLine(2 * k - 1);
+    }
+    else
+    {
+        Console.WriteLine(2 * (k - (n / 2)));
+    }
+}
+else
+{
+    if (((n+1) / 2) >= k)
+    {
+        Console.WriteLine(2 * k - 1);
+    }
+    else
+    {
+        Console.WriteLine(2 * (k - ((n+1) / 2)));
+    }
+}
+
---
+++ codeforces/A. Is your horseshoe on the other hoof/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] values = Console.ReadLine().Split(' ');
+int count = 0;
+string c = "a";
+for(int i=0;i<values.Length-1;i++)
+{
+    if (c == values[i])
+        continue;
+    for(int j=i+1;j<values.Length;j++)
+    {
+        if (values[i] == values[j])
+            count++;
+    }
+    c = values[i];
+}
+Console.WriteLine(count);
